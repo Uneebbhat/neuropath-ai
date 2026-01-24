dc9b8435bc40e0bdf1af4d7f3b0607f7ac49e9c6
UneebBhatti <uneebbhatti3@gmail.com>
2026-01-24 20:09:12 +0500

    Auth pages created

---
+++ src/modules/(auth)/forgot-password/components/forgot-password-form.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+
+export function ForgotPasswordForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Forgot Password</CardTitle>
+          <CardDescription>
+            Enter your email to reset your password
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <Button>Send email</Button>
+                <FieldDescription className="text-center">
+                  <Link href="/login">Back to login</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- src/modules/(auth)/reset-password/components/reset-password-form.tsx
+++ src/modules/(auth)/reset-password/components/reset-password-form.tsx
@@ -16,44 +16,29 @@ import {
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import Link from "next/link"
 import { Eye, EyeOff } from "lucide-react"
 import useTogglePassword from "@/hooks/useTogglePassword"
-import Link from "next/link"
 
-export function SignupForm({
+export function ResetPasswordForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
   const { showPassword, handleTogglePassword } = useTogglePassword()
-
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Create your account</CardTitle>
+          <CardTitle className="text-xl">Reset Password</CardTitle>
           <CardDescription>
-            Enter your email below to create your account
+            Enter your new password to reset your password
           </CardDescription>
         </CardHeader>
         <CardContent>
           <form>
             <FieldGroup>
               <Field>
-                <FieldLabel htmlFor="name">Full Name</FieldLabel>
-                <Input id="name" type="text" placeholder="John Doe" name="name" required />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                  name="email"
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <FieldLabel htmlFor="password">New Password</FieldLabel>
                 <div className="relative">
                   <Input id="password" type={showPassword ? "text" : "password"} placeholder="password" name="password" required />
                   {
@@ -63,23 +48,16 @@ export function SignupForm({
                   }
                 </div>
               </Field>
-              <FieldDescription>
-                Must be at least 8 characters long.
-              </FieldDescription>
               <Field>
-                <Button>Create Account</Button>
+                <Button>Reset Password</Button>
                 <FieldDescription className="text-center">
-                  Already have an account? <Link href="/login">Login</Link>
+                  <Link href="/login">Back to login</Link>
                 </FieldDescription>
               </Field>
             </FieldGroup>
           </form>
         </CardContent>
       </Card>
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <Link href="/terms-of-service">Terms of Service</Link>{" "}
-        and <Link href="/privacy-policy">Privacy Policy</Link>.
-      </FieldDescription>
     </div>
   )
 }
